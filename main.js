b963949d9480989e9fd6c7ba2e09a8556d597198
keiswara61 <keiswara61@gmail.com>
2026-01-30 10:01:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ document.getElementById("tombolSuka").addEventListener("click", telahSuka);
 function telahSuka() {
   document.getElementById("tombolSuka").disabled = true;
   document.getElementById("tombolSuka").textContent = "Menyukai";
-  document.getElementById("telahSuka").textContent = "Terima kasih telah menyukai halaman GitHub saya!";
+  document.getElementById("telahSuka").textContent = "Terima kasih telah menyukai halaman GitHub saya! &#2764;";
 }
