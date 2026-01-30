7620dea5d943ad9d7b259f9f2ec8c4d3f1d0d772
keiswara61 <keiswara61@gmail.com>
2026-01-30 18:40:04 +0700

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("tombolSuka").addEventListener("click", telahSuka);
-function telahSuka() {
-  document.getElementById("tombolSuka").disabled = true;
-  document.getElementById("tombolSuka").textContent = "Menyukai";
-  document.getElementById("telahSuka").textContent = "Terima kasih telah menyukai halaman GitHub saya! ❤️";
-}
